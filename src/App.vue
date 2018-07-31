fa3afefa7e75e4018bc7627870d013eed4f9326f
Milos <miloske1104@gmail.com>
2018-07-31 20:56:04 +0200

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <el-input placeholder="Please input" v-model="input" class="input"></el-input>
+    <i class="el-icon-edit"></i>
+    <i class="el-icon-share"></i>
+    <i class="el-icon-delete"></i>
+    <el-button type="primary" icon="el-icon-search">Search</el-button>
+    <el-button class="loader" plain type="primary" v-loading="loading" @click='triggerLoading()'>Load</el-button>
   </div>
 </template>
 
@@ -12,6 +16,19 @@ export default {
   name: 'app',
   components: {
     HelloWorld
+  },
+
+  data() {
+    return {
+      input: '',
+      loading: false
+    }
+  },
+
+  methods: {
+    triggerLoading: function() {
+      this.loading = true;
+    }
   }
 }
 </script>
@@ -24,5 +41,14 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.input {
+  width: 200px;
+}
+
+.loader:hover {
+  background-color: #303133;
+  color: white;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
 import Vue from 'vue'
+import Element from 'element-ui';
+import ElementUI from 'element-ui';
+import '../theme/index.css'
 import App from './App.vue'
 
 Vue.config.productionTip = false
+
+Vue.use(ElementUI);
+Vue.use(Element, { size: 'small', zIndex: 3000 });
 
 new Vue({
   render: h => h(App)
